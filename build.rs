c63f61d2eaa1e8d39bcdea04f7781bbce297e344
TaurusM <3075985899@qq.com>
2023-11-04 18:53:30 +0800

    add type attr

--- build.rs
+++ build.rs
@@ -2,14 +2,17 @@
 
 fn main() -> Result<(), std::io::Error> {
     // env::set_var("OUT_DIR", "src");
-    tonic_build::configure().build_server(true).compile(
-        &[
-            "dapr/proto/common/v1/common.proto",
-            "dapr/proto/runtime/v1/dapr.proto",
-            "dapr/proto/runtime/v1/appcallback.proto",
-            "examples/invoke/proto/helloworld.proto",
-        ],
-        &["."],
-    )?;
+    tonic_build::configure()
+        .type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]")
+        .build_server(true)
+        .compile(
+            &[
+                "dapr/proto/common/v1/common.proto",
+                "dapr/proto/runtime/v1/dapr.proto",
+                "dapr/proto/runtime/v1/appcallback.proto",
+                "examples/invoke/proto/helloworld.proto",
+            ],
+            &["."],
+        )?;
     Ok(())
 }
